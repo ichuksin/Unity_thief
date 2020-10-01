bf341bebce4ffa8bc8f54cb57b08f9f9b52294a5
ichuksin <i.chuksin@gmail.com>
2020-10-01 22:15:06 +0300

    Init

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        Vector2 contactPOint = collider.ClosestPoint(new Vector2(0, 0));
+        Debug.Log(contactPOint.x);
+    }
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        Vector2 contactPOint = collider.ClosestPoint(new Vector2(0, 0));
+        Debug.Log("Выход " + contactPOint.x);
+    }
+}
